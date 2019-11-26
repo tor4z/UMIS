9deece82e4213a27bde064a467604f02428c5785
tor4z <vwenjie@hotmail.com>
2019-11-26 23:28:30 +0800

    Update summary

--- utils/summaries.py
+++ utils/summaries.py
@@ -6,29 +6,63 @@ from torch.utils import tensorboard
 
 class Summary(object):
     def __init__(self, opt):
-        path = os.path.join(opt.summary_dir, opt.runtime_id)
-        self.dir = path
+        self.dir = os.path.join(opt.summary_dir, opt.runtime_id)
         self.writer = tensorboard.SummaryWriter(log_dir=path)
+        self.thredhold = opt.thredhold
+        self.z = np.random.randint(0, image.shape[2])
+        self.disp_images = opt.disp_images
 
-    def add_image(self, writer, image, output, global_step):
-        z = np.random.randint(0, image.shape[2])
+    def add_image(self, tag, image, global_step):
+        self.writer.add_imag(tag, image, global_steps)
 
-        grid_image = make_grid(image[:3, :, z, :, :].clone().cpu().data, 3, normalize=True)
-        writer.add_image('Image', grid_image, global_step)
+    def train_image(self, input, seg, rec, global_steps):
+        # display input image
+        grid_image = make_grid(input[:self.disp_images, :, self.z, :, :].clone().cpu().data,
+                                self.disp_images, normalize=True)
+        self.add_image('train/input', grid_image, global_step)
 
-        grid_image = make_grid(output[:3, :, z, :, :].clone().cpu().data, 3, normalize=True)
-        writer.add_image('Output', grid_image, global_step)
+        # display seg image
+        grid_image = make_grid(seg[:self.disp_images, :, self.z, :, :].clone().cpu().data,
+                                self.disp_images, normalize=True)
+        self.add_image('train/seg', grid_image, global_step)
 
-        output[output > 0.9] = 1
-        output[output <= 0.9] = 0
-        grid_image = make_grid(output[:3, :, z, :, :].clone().cpu().data, 3, normalize=True)
-        writer.add_image('Output - mask', grid_image, global_step)
+        # display seg image with thredhold
+        seg[seg > self.thredhold] = 1
+        seg[seg <= self.thredhold] = 0
+        grid_image = make_grid(seg[:self.disp_images, :, self.z, :, :].clone().cpu().data, 
+                                self.disp_images, normalize=True)
+        writer.add_image('train/seg/thredhold', grid_image, global_step)
 
-    def add_text(self):
-        pass
+        # display rec image
+        grid_image = make_grid(rec[:self.disp_images, :, self.z, :, :].clone().cpu().data,
+                                self.disp_images, normalize=True)
+        writer.add_image('train/rec', grid_image, global_step)
 
-    def add_scalars(self):
-        pass
+    def val_image(self, input, seg, global_steps):
+        # display input image
+        grid_image = make_grid(input[:self.disp_images, :, self.z, :, :].clone().cpu().data,
+                                self.disp_images, normalize=True)
+        self.add_image('val/input', grid_image, global_step)
+
+        # display seg image
+        grid_image = make_grid(seg[:self.disp_images, :, self.z, :, :].clone().cpu().data,
+                                self.disp_images, normalize=True)
+        self.add_image('val/seg', grid_image, global_step)
+
+        # display seg image with thredhold
+        seg[seg > self.thredhold] = 1
+        seg[seg <= self.thredhold] = 0
+        grid_image = make_grid(seg[:self.disp_images, :, self.z, :, :].clone().cpu().data,
+                                    self.disp_images, normalize=True)
+        writer.add_image('val/seg/thredhold', grid_image, global_step)
+
+    def add_text(self, tag, text, global_steps):
+        self.writer.add_text(tag, text, global_steps)
+        self.flush()
+
+    def add_scalars(self, tag, scalars, global_steps):
+        self.writer.add_scalars(tag, scalars, global_steps)
+        self.flush()
 
     def flush(self):
         self.writer.flush()
